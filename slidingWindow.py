6e7c03d70d5f1e3edba698d894411ff68c6b1e55
Artem Rychenkov <fleshmercy@gmail.com>
2022-10-01 19:20:57 +0300

    Update slidingWindow for oversize images

--- slidingWindow.py
+++ slidingWindow.py
@@ -1,13 +1,20 @@
-from email.generator import Generator
 import cv2
+import numpy as np
 from typing import Iterator, Tuple
 
 
 def SlidingWindow(image: cv2.Mat, slideSize: Tuple[int, int]) -> Iterator[Tuple[cv2.Mat, int, int]]:
+    extraY = image.shape[0] % slideSize[1]
+    extraX = image.shape[1] % slideSize[0]
+    if extraY != 0:
+        extraBlockBottom = np.zeros((slideSize[1] - extraY, image.shape[1], 3), np.uint8)
+        image = np.concatenate((image, extraBlockBottom), axis=0)
+    if extraX != 0:
+        extraBlocRight = np.zeros((image.shape[0], slideSize[0] - extraX, 3), np.uint8)
+        image = np.concatenate((image, extraBlocRight), axis=1)
+    
     for y in range(0, image.shape[0], slideSize[1]):
-        if y + slideSize[1] > image.shape[0]: break   
         for x in range(0, image.shape[1], slideSize[0]):
-            if x + slideSize[0] > image.shape[1]: break      
             yield (image[y: y + slideSize[1], x: x + slideSize[0]], x, y + slideSize[1])
 
 if __name__ == "__main__":
